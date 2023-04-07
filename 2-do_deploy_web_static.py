4ac750f288d3ff00c8c0d269b5eff5c8d958a281
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-07 02:27:27 +0100

    fix: :bug: toatl refactoring

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,21 +14,21 @@ def do_deploy(archive_path):
     """Deploys the web static to the server"""
     if not exists(archive_path):
         return False
+    results = []
+    res = put(archive_path, "/tmp")
+    results.append(res.succeeded)
 
-    try:
-        archiveWithExt = archive_path.split("/")[-1]
-        archiveNoExt = archiveWithExt.split(".")[0]
-        releaseVersion = "/data/web_static/releases/{}/".format(archiveNoExt)
-        symLink = "/data/web_static/current"
+    basename = os.path.basename(archive_path)
+    if basename[-4:] == ".tgz":
+        name = basename[:-4]
+    newdir = "/data/web_static/releases/" + name
+    run("mkdir -p " + newdir)
+    run("tar -xzf /tmp/" + basename + " -C " + newdir)
 
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releaseVersion))
-        run("tar -xzf /tmp/{} -C {}".format(archiveWithExt, releaseVersion))
-        run("rm -rf /tmp/{}".format(archiveWithExt))
-        run("mv {0}web_static/* {0}".format(releaseVersion))
-        run("rm -rf {}web_static".format(releaseVersion))
-        run("rm -rf {}".format(symLink))
-        run("ln -s {} {}".format(releaseVersion, symLink))
-        return True
-    except:
-        return False
+    run("rm /tmp/" + basename)
+    run("mv " + newdir + "/web_static/* " + newdir)
+    run("rm -rf " + newdir + "/web_static")
+    run("rm -rf /data/web_static/current")
+    run("ln -s " + newdir + " /data/web_static/current")
+
+    return True
