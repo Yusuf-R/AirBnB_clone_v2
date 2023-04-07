c490d032cf351923318e1909bf6cd1864485f856
Yusuf-R <y.abdulwasiu@gmail.com>
2023-04-07 02:07:26 +0100

    feat: :sparkles: compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This a Fabric script to generate a .tgz
+archive from the contents of the web_static
+"""
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """This method will compress web_static folder"""
+
+    local("mkdir -p versions")
+    currentTime = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(currentTime)
+    local("tar -cvzf {} web_static".format(path))
+    if local("[ -f versions/web_static_{}.tgz ]".format(currentTime)).strip():
+        return path
