817ec8a0f467b7f59fbd1522c03778e4e3cca68b
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-15 13:51:30 +0100

    feat: :sparkles: creates and distributes an archive to our web servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,13 +3,13 @@
 Fabric script to generate a .tgz archive
 The contents the web_static will be moved to the servers
 """
-
-from fabric.api import local, put, env, run
+from fabric.api import local, cd, sudo, put, env
 from datetime import datetime
 from os.path import exists
 
 env.hosts = ["54.160.90.38", "100.25.36.19"]
 env.user = "ubuntu"
+
 
 def do_pack():
     """Compress the web_static folder"""
@@ -25,26 +25,66 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Deploys the web static to the server"""
+    """
+    Function to deploy our static files
+
+    Args:
+        archive_path: path to the archive
+    Returns:
+        False:
+            if file at the path doesn't exist
+        True:
+            if all operations were done correctly
+    Raises:
+        None
+    """
     if not exists(archive_path):
         return False
 
     try:
-        archiveWithExt = archive_path.split("/")[-1]
-        archiveNoExt = archiveWithExt.split(".")[0]
-        releaseVersion = "/data/web_static/releases/{}/".format(archiveNoExt)
-        symLink = "/data/web_static/current"
+        # filename with .tgz
+        file_tgz = archive_path.split("/")[-1]
 
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releaseVersion))
-        run("tar -xzf /tmp/{} -C {}".format(archiveWithExt, releaseVersion))
-        run("rm -rf /tmp/{}".format(archiveWithExt))
-        run("mv {0}web_static/* {0}".format(releaseVersion))
-        run("rm -rf {}web_static".format(releaseVersion))
-        run("rm -rf {}".format(symLink))
-        run("ln -s {} {}".format(releaseVersion, symLink))
-        return True
+        # to get the filename without tgz
+        file = file_tgz.split(".")[0]
+
+        # download destination for my put command
+        dest = "/tmp/"
+
+        # set archive destination
+        arch_dest = "/data/web_static/releases/{}".format(file)
+
+        # sym link to my archive dest
+        symbolic_link = "/data/web_static/current"
+
+        # start message
+        print("Deploying {} to our server".format(file))
+
+        # upload the archive to the /tmp/ dir of the server
+        put(archive_path, dest)
+
+        with cd("/tmp/"):
+            # uncompres the tgz to the given dest
+            # first we create the destination as it doesn't
+            # exist by default
+            sudo("mkdir -p {}".format(arch_dest))
+
+            # uncompress to the destnaion created
+            sudo("tar -xzf {} -C {} --strip-components=1"
+                 .format(file_tgz, arch_dest))
+
+            # delete the tgz file
+            sudo("rm -rf {}".format(file_tgz))
+
+        # remove old link file
+        sudo("rm -rf {}".format(symbolic_link))
+
+        # ln -s link_source link_name
+        sudo("ln -s {} {}".format(arch_dest, symbolic_link))
+        print("All done")
+
     except Exception:
+        print("Failed to deploy succesfully")
         return False
 
 
@@ -53,5 +93,4 @@ def deploy():
     archivePath = do_pack()
     if not archivePath:
         return False
-
     return do_deploy(archivePath)
