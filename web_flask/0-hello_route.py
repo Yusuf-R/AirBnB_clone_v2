e17d8332d1c4a7a53f92b659b353d0e026c87f2e
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-22 02:19:53 +0100

    feat: :sparkles: jut dynamics

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,11 @@
 #!/usr/bin python3
 """This script will start a flask application"""
-
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnh():
     """Hello HBNB!"""
     return 'Hello HBNB!'
 
