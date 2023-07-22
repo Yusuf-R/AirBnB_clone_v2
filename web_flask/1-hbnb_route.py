31b2b1f42fe81fc005247a96693e4748e7b5f846
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-22 04:44:27 +0100

    feat: :sparkles: adding a new index

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin python3
+"""This script will start a flask application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index_hbnb():
+    """displays HBNB!"""
+    return 'HBNB!'
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000)
