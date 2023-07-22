2d7c123124be2d62142dc86ae4f36a8be56d4741
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-22 08:17:12 +0100

    fix: :bug: added a another route for the instance

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This script will start a flask application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Hello HBNB!"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def index_hbnb():
+    """displays HBNB!"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def index_c_text(text):
+    """displays C followed by the value of the text variable"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def index_python_text(text):
+    """Display 'Python ', followed by the value of the text variable"""
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000, debug=True)
