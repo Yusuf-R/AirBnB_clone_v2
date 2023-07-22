72e2a9fd94027e4451363d7ad053d795ee281511
Yusuf-R <y.abdulwasiu@gmail.com>
2023-07-22 02:06:45 +0100

    feat: :sparkles: using flask deve kits

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin python3
+"""This script will start a flask application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000)
